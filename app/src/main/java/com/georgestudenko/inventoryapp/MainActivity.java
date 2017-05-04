7f85e0bc9f0e5741359c04057f3336185686afc6
George Studenko <gnstudenko@gmail.com>
2017-05-05 00:10:09 +0200

    feat: Add initial code to implement CursorLoader on MainActivity

--- app/src/main/java/com/georgestudenko/inventoryapp/MainActivity.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.georgestudenko.inventoryapp;
 
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
