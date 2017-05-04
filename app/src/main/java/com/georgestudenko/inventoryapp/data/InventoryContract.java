092fe6447e22ea19269615905688ffae4f29737a
George Studenko <gnstudenko@gmail.com>
2017-05-05 00:13:44 +0200

    feat: Add CONTENT_URI  to InventoryEntry

--- app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryContract.java
@@ -9,7 +9,12 @@ import android.provider.BaseColumns;
 
 public final class InventoryContract {
 
+    public static final String CONTENT_AUTHORITY = "com.georgestudenko.inventoryapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_INVENTORY = "inventory";
+
     public final static class InventoryEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
         public final static String TABLE_NAME = "inventory";
         public final static String COLUMN_ID = BaseColumns._ID;
         public final static String COLUMN_PRODUCT_NAME = "name";
