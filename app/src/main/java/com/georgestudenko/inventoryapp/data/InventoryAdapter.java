b3ec1ed2e43b1f8d7c08e5bb96f8c85092f2539d
George Studenko <gnstudenko@gmail.com>
2017-05-05 00:48:17 +0200

    feat: Extend CursorAdapter  and override methods

--- app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryAdapter.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryAdapter.java
@@ -1,8 +1,28 @@
 package com.georgestudenko.inventoryapp.data;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
 /**
  * Created by george on 04/05/2017.
  */
 
-public class InventoryAdapter {
+public class InventoryAdapter extends CursorAdapter {
+
+    public InventoryAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
 }
