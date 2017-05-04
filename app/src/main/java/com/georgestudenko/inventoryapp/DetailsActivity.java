3df00675dfc74aba61b390fbdd8a6e63bbb833db
George Studenko <gnstudenko@gmail.com>
2017-05-05 01:00:37 +0200

    feat: Implement CursorLoader on DetailsActivity

--- app/src/main/java/com/georgestudenko/inventoryapp/DetailsActivity.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/DetailsActivity.java
@@ -1,13 +1,31 @@
 package com.georgestudenko.inventoryapp;
 
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity  implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
