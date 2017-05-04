08986ab6702b8d93f26748f64f655cfc0c4d5b02
George Studenko <gnstudenko@gmail.com>
2017-05-05 00:12:50 +0200

    feat: Add InventoryEntry  subclass

--- app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryContract.java
@@ -1,8 +1,19 @@
 package com.georgestudenko.inventoryapp.data;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by george on 04/05/2017.
  */
 
 public class InventoryContract {
+    public final static class InventoryEntry implements BaseColumns {
+        public final static String TABLE_NAME = "inventory";
+        public final static String COLUMN_ID = BaseColumns._ID;
+        public final static String COLUMN_PRODUCT_NAME = "name";
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+        public final static String COLUMN_QUANTITY = "qty";
+        public final static String COLUMN_PRODUCT_DESCRIPTION = "description";
+    }
 }
