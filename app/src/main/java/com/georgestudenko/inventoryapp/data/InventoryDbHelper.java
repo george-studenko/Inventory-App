06b0e4ef14b55a4def29e22ccb7865bd8db38fc3
George Studenko <gnstudenko@gmail.com>
2017-05-05 00:47:40 +0200

    feat: Extend SQLiteOpenHelper  and override methods

--- app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryDbHelper.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryDbHelper.java
@@ -1,8 +1,26 @@
 package com.georgestudenko.inventoryapp.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by george on 04/05/2017.
  */
 
-public class InventoryDbHelper {
+public class InventoryDbHelper extends SQLiteOpenHelper {
+
+    public InventoryDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
