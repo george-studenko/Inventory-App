01518aa126601a6faa6d1d22400ceaa819ca2b45
George Studenko <gnstudenko@gmail.com>
2017-05-05 16:47:04 +0200

    feat: Implement onCreate on InventoryDbHelper

--- app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryDbHelper.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryDbHelper.java
@@ -4,11 +4,22 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.georgestudenko.inventoryapp.data.InventoryContract.InventoryEntry;
+
 /**
  * Created by george on 04/05/2017.
  */
 
 public class InventoryDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "inventory.db";
+    public static final int DATABASE_VERSION = 1;
+    public final String SQL_CREATE_INVENTORY_TABLE = "CREATE TABLE " + InventoryEntry.TABLE_NAME + "("+
+            InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            InventoryEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL,"+
+            InventoryEntry.COLUMN_PRODUCT_DESCRIPTION + " TEXT," +
+            InventoryEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL,"+
+            InventoryEntry.COLUMN_QUANTITY + " INTEGER NOT NULL)";
 
     public InventoryDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -16,7 +27,7 @@ public class InventoryDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_INVENTORY_TABLE);
     }
 
     @Override
