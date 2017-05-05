1e60121e508027172e75077942168d747beaccb1
George Studenko <gnstudenko@gmail.com>
2017-05-05 17:54:38 +0200

    feat: Implement newView on InventoryAdapter

--- app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryAdapter.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryAdapter.java
@@ -2,9 +2,14 @@ package com.georgestudenko.inventoryapp.data;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.georgestudenko.inventoryapp.R;
+import com.georgestudenko.inventoryapp.model.InventoryItem;
 
 /**
  * Created by george on 04/05/2017.
@@ -18,7 +23,8 @@ public class InventoryAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.inventory_item,parent,false);
+        return view;
     }
 
     @Override
