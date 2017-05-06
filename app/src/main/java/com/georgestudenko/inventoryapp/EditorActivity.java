1f528f33a4a702d979d7806719708b2c3cdc62f3
George Studenko <gnstudenko@gmail.com>
2017-05-06 08:42:45 +0200

    feat: Add menu to EditorActivity

--- app/src/main/java/com/georgestudenko/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/EditorActivity.java
@@ -2,6 +2,8 @@ package com.georgestudenko.inventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.editor_menu,menu);
+        return true;
     }
 }
