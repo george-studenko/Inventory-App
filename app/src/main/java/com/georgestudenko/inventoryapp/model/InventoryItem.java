229683f684cc0891d9d17abc2b456da3d447fe4b
George Studenko <gnstudenko@gmail.com>
2017-05-07 15:12:04 +0200

    feat: Add Id to InventoryItem model class

--- app/src/main/java/com/georgestudenko/inventoryapp/model/InventoryItem.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/model/InventoryItem.java
@@ -2,7 +2,10 @@ package com.georgestudenko.inventoryapp.model;
 
 import android.database.Cursor;
 
+import com.georgestudenko.inventoryapp.R;
 import com.georgestudenko.inventoryapp.data.InventoryContract;
+
+import java.text.DecimalFormat;
 
 /**
  * Created by george on 05/05/2017.
@@ -14,16 +17,22 @@ public class InventoryItem {
     private String mDescription;
     private int mQuantity;
     private int mPrice;
+    private long mId;
 
-    public InventoryItem(String name, String description, int quantity, int price) {
+    public InventoryItem(String name, String description, int quantity, int price, long id) {
         mName = name;
         mDescription = description;
         mQuantity = quantity;
         mPrice = price;
+        mId = id;
     }
 
     public String getName() {
         return mName;
+    }
+
+    public long getId() {
+        return mId;
     }
 
     public String getDescription() {
@@ -43,11 +52,16 @@ public class InventoryItem {
     }
 
     public static InventoryItem parseInventoryItem(Cursor cursor){
-        InventoryItem item = new InventoryItem(cursor.getString(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_NAME)),
-                cursor.getString(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_DESCRIPTION)),
-                cursor.getInt(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_QUANTITY)),
-                cursor.getInt(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_PRICE)));
+        if(cursor!=null) {
+            InventoryItem item = new InventoryItem(cursor.getString(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_NAME)),
+                    cursor.getString(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_DESCRIPTION)),
+                    cursor.getInt(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_QUANTITY)),
+                    cursor.getInt(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_PRICE)),
+                    cursor.getInt(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_ID)));
+            return item;
+        }else{
+            return null;
+        }
 
-        return item;
     }
 }
