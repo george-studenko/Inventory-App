4d5d2d767be8fad9849488b5a4794f71dbd2ef46
George Studenko <gnstudenko@gmail.com>
2017-05-07 14:27:15 +0200

    feat: Add  sell button to inventory item

--- app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryAdapter.java
+++ app/src/main/java/com/georgestudenko/inventoryapp/data/InventoryAdapter.java
@@ -1,15 +1,23 @@
 package com.georgestudenko.inventoryapp.data;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.georgestudenko.inventoryapp.R;
 import com.georgestudenko.inventoryapp.model.InventoryItem;
+
+import static java.security.AccessController.getContext;
 
 /**
  * Created by george on 04/05/2017.
@@ -32,11 +40,25 @@ public class InventoryAdapter extends CursorAdapter {
         TextView name = (TextView) view.findViewById(R.id.itemName);
         TextView qty = (TextView) view.findViewById(R.id.itemQuantity);
         TextView price = (TextView) view.findViewById(R.id.itemPrice);
+        ImageButton sellButton = (ImageButton)view.findViewById(R.id.sellListButton);
 
         InventoryItem item = InventoryItem.parseInventoryItem(cursor);
 
         name.setText(item.getName());
         qty.setText(item.getQuantity());
         price.setText(item.getFormattedPrice());
+        sellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(item.getQuantity()>0) {
+                    ContentValues cv = new ContentValues();
+                    cv.put(InventoryContract.InventoryEntry.COLUMN_QUANTITY,item.getQuantity()-1);
+                    v.getContext().getContentResolver().update(ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI, item.getId()), cv, null, null);
+                    Toast.makeText(v.getContext(),"Sold 1 unit of " + item.getName() +" for "+ item.getFormattedPrice(),Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(v.getContext(),"Impossible to make a sale stock is 0",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
